a9c70d1f43e1a872a16537b2745df8b4c35e38f2
Olesia Lisichnikova <alesya.mail@ya.ru>
2024-05-28 00:27:55 +0300

    Fixes: conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,21 +5,26 @@ from helpers import *
 @allure.step('Генерация данных для курьера, логин, удаление курьера')
 @pytest.fixture
 def generate_courier_then_delete():
-    # Данные для создания курьера
     new_courier_data = generate_new_courier_data()
-    login_pass = [new_courier_data[0], new_courier_data[1], new_courier_data[2]]
-
+    login_pass = extract_login_pass(new_courier_data)
     yield login_pass
+    user_id = get_user_id(new_courier_data)
+    delete_courier(user_id)
 
-    # Логин курьера
+
+def extract_login_pass(new_courier_data):
+    login_pass = [new_courier_data[0], new_courier_data[1], new_courier_data[2]]
+    return login_pass
+
+
+def get_user_id(new_courier_data):
     payload = {
         'login': new_courier_data[0],
         'password': new_courier_data[1]
     }
     login_response = post_login_courier(payload)
-    # Удалить курьера
     user_id = login_response.json()['id']
-    delete_courier(user_id)
+    return user_id
 
 
 @allure.step('Генерация данных для курьера, регистрация, удаление курьера')
@@ -34,16 +39,7 @@ def generate_courier_register_then_delete():
     registration_response = post_create_courier(payload)
     login_pass = []
     if registration_response.status_code == 201:
-        login_pass.append(new_courier_data[0])
-        login_pass.append(new_courier_data[1])
-        login_pass.append(new_courier_data[2])
-
-    yield login_pass
-
-    payload = {
-        'login': new_courier_data[0],
-        'password': new_courier_data[1]
-    }
-    login_response = post_login_courier(payload)
-    user_id = login_response.json()['id']
-    delete_courier(user_id)
+        login_pass = extract_login_pass(new_courier_data)
+        yield login_pass
+        user_id = get_user_id(new_courier_data)
+        delete_courier(user_id)
